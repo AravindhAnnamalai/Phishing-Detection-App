ac99732db886b8ad506468d68a8617c9a440f63e
AravindhAnnamalai <111548774+AravindhAnnamalai@users.noreply.github.com>
2024-04-10 22:56:51 +0530

    app.py

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 import pickle
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
-import re
-
-def is_potential_url(string):
-    # Regular expression pattern to match URL-like strings
-    url_pattern = re.compile(
-        r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
-    )
-    return bool(re.match(url_pattern, string))
-
 
 # Load the tokenizer from the pickle file
 with open('tokenizer.pkl', 'rb') as f:
@@ -35,7 +26,7 @@ def main():
     
 
     if st.button('Check'):
-        if is_potential_url(url):
+        if url:
             st.write("Checking...")
             seq = tokenize_url(url, tokenizer)
             pred_val = model.predict(seq)
@@ -44,7 +35,7 @@ def main():
             else:
                 st.success('It is a Legitimate Website.')
         else:
-            st.write("It's not a proper URL")
+            st.warning("Please enter a URL.")
 
 if __name__ == "__main__":
     main()
