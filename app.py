6cfcbc1ebe77fd898c9a6a861aafe1341cccbbcc
AravindhAnnamalai <111548774+AravindhAnnamalai@users.noreply.github.com>
2024-04-12 16:35:10 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
-
+import re
 # Load the tokenizer from the pickle file
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
@@ -17,6 +17,19 @@ def tokenize_url(url, tokenizer, sequence_length=200):
     padded_sequence = pad_sequences(url_sequence, maxlen=sequence_length)
     return padded_sequence
 
+
+def is_valid_url(url):
+    # Regular expression pattern to match a URL
+    url_pattern = re.compile(
+        r'^(https?://)?'  # Scheme (http:// or https://)
+        r'((([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})'  # Domain name
+        r'|localhost)'  # localhost
+        r'(:[0-9]{1,5})?'  # Port number
+        r'(/?|/[\w#!:.?+=&%@!\-/]*)?$'  # Path
+    )
+    return bool(url_pattern.match(url))
+
+
 def main():
     st.title("Phishing URL Checker")
     st.write("Enter the URL below to check if it's phishing or legitimate.")
@@ -27,15 +40,18 @@ def main():
 
     if st.button('Check'):
         if url:
-            st.write("Checking...")
-            seq = tokenize_url(url, tokenizer)
-            pred_val = model.predict(seq)
-            if pred_val > 0.5:
-                st.error('It is a Phishing Website')
+            if is_valid_url(url):
+                st.write("Checking...")
+                seq = tokenize_url(url, tokenizer)
+                pred_val = model.predict(seq)
+                if pred_val > 0.5:
+                    st.error('It is a Phishing Website')
+                else:
+                    st.success('It is a Legitimate Website.')
             else:
-                st.success('It is a Legitimate Website.')
+                st.warning('Please enter a valid URL.')
         else:
             st.warning("Please enter a URL.")
 
-if __name__ == "__main__":
-    main()
+if _name_ == "_main_":
+    main()
